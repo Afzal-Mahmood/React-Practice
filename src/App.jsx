80972627217b9d976fdf69d8eba4054d0d028ed4
Afzal Mahmood <afzalmehmood586@gmail.com>
2026-07-24 23:31:47 +0500

    Today i wil Start react js

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Sum from "./Sum"
+
+function App() {
+
+
+  let userName = "AFZAL MAHMOOD"
+
+  return (
+    <>
+      <h2>HELLO ! </h2>
+      <p>This is my First day practice on React {userName} </p>
+      <Sum />
+    </>
+  )
+}
+
+export default App
---
+++ src/Sum.jsx
@@ -0,0 +1,15 @@
+function Sum() {
+    let a = 50;
+    let b = 150;
+    function Add() {
+        return a + b ;
+    }
+   return(
+        <>
+            <h3>The Sum of these {a} & {b} two Number = {Add()}</h3>
+            <h1>HELLO !</h1>
+        </>
+   
+   )
+} 
+export default Sum
